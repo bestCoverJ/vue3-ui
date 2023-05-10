85e8b640f875423c415ab6b150d3e13365339b2f
coverj009 <win8yh1@outlook.com>
2023-05-10 23:35:41 +0800

    修改代码行样式

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-import 'highlight.js/styles/atom-one-light.css'
+import '@/assets/code/atom-one-light.css'
 import 'highlight.js/lib/common'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
-
-hljs.registerLanguage('javascript', javascript);
 
 import '@/assets/main.scss'
 
